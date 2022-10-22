a867bb8dc1d478a2411e0b92b1ad427ebb137327
CharlesPogiii <gonzalescharles0702@gmail.com>
2022-10-23 02:02:01 +0800

    cart xpress-last edited-search-manager

--- layout/resources/js/mobile/nav.js
+++ layout/resources/js/mobile/nav.js
@@ -1,8 +1,16 @@
-function myFunction() {
+function navBar() {
     var mobMenu = document.getElementById("mobMenu");
     if (mobMenu.style.display === "block") {
       mobMenu.style.display = "none";
     } else {
       mobMenu.style.display = "block";
+    }
+  }
+  function sortButton() {
+    var sortButton = document.getElementById("sortButton");
+    if (sortButton.style.display === "block") {
+      sortButton.style.display = "none";
+    } else {
+      sortButton.style.display = "block";
     }
   }
