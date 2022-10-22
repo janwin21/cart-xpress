76f99417fa365e16b2f2290447b0a62e673c6050
CharlesPogiii <gonzalescharles0702@gmail.com>
2022-10-22 13:00:38 +0800

    cart xpress_ header responsive

---
+++ layout/resources/js/mobile/nav.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var mobMenu = document.getElementById("mobMenu");
+    if (mobMenu.style.display === "block") {
+      mobMenu.style.display = "none";
+    } else {
+      mobMenu.style.display = "block";
+    }
+  }
